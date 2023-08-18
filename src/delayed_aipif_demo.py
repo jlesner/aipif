ee1f651eeba744b72e265aedba5ee11752343d29
jlesner <jlesner@ucsc.edu>
2023-08-17 23:37:56 -0700

    added multithreaded bounded buffer for delayed demo api tasks

--- src/delayed_aipif_demo.py
+++ src/delayed_aipif_demo.py
@@ -1,11 +1,12 @@
-import logging
+import time
 import random
+import threading
 from Context import Context
+from thread_management import Model_Task, BoundedBuffer, worker
 from pictures.DelayedPictureMaker import DelayedPictureMaker
 from sounds.DelayedSoundMaker import DelayedSoundMaker
 from music.DelayedMusicMaker import DelayedMusicMaker
 from text.DelayedTextMaker import DelayedTextMaker
-
 
 def context_configure(context:Context):
     # if your module needs to be configured, you can do it here
@@ -14,27 +15,38 @@ def context_configure(context:Context):
     # WARNING: DO NOT CHECKIN YOUR API KEYS / SECRETS
     #
     context.config['story_maker_port'] = 8080
+    context.config['sentinel'] = "STOP"
+    context.config['num_workers'] = 5
     
 
 def state_setup(context:Context):
     # setup context.state using context.config 
     
     # for now we hardcode function bindings
+    context.state['todo_tasks'] = BoundedBuffer(10)
+    context.state['done_tasks'] = BoundedBuffer(10)
+
     context.state['make_picture'] = DelayedPictureMaker(context).make_picture
     context.state['make_sound'] = DelayedSoundMaker(context).make_sound
     context.state['make_music'] = DelayedMusicMaker(context).make_music
     context.state['make_story'] = DelayedTextMaker(context).make_text
 
-def activity_simulate(context:Context):
-    for i in range(50):
-        activity = random.choice(['make_picture', 'make_sound', 'make_music', 'make_story'])        
-        context.state[activity]({"positive_prompt_text": f"iteration {i}"})
-        print(f"{activity} iteration {i}")
-
+def task_manager(context:Context):
+    for i in range(5):
+        activity = random.choice(['make_picture', 'make_sound', 'make_music', 'make_story'])
+        # context.state[activity]({"positive_prompt_text": f"iteration {i}"})
+        task = Model_Task(process_function=context.state[activity], prompt_dict={"positive_prompt_text": f"iteration {i}"})
+        context.state['todo_tasks'].add(task)
+        print(f"Manager producing {task.process_function.__name__} for {task.input_dict}")
+        time.sleep(random.random())
+    print("Manager done!")
+    context.state['todo_tasks'].add(context.config['sentinel'])
 
 if __name__ == '__main__':
-    logging.basicConfig(filename=__file__[:-3] +'.log', filemode='w', level=logging.DEBUG)
     context = Context()
     context_configure(context)
     state_setup(context)
-    activity_simulate(context)
+    print("starting tasks")
+    threading.Thread(target=task_manager, args=(context,)).start()
+    for i in range(context.config['num_workers']):
+        threading.Thread(target=worker, args=(context,i)).start()
---
+++ src/thread_management.py
@@ -0,0 +1,55 @@
+import threading
+
+class Model_Task:
+    def __init__(self, process_function=None, prompt_dict={}): # not sure if should be None or empty dict
+        self.process_function = process_function
+        self.input_dict = prompt_dict
+        self.output = None
+
+class BoundedBuffer:
+
+    def __init__(self, size):
+        self.buffer = [None] * size
+        self.size = size
+        self.count = 0  
+        self.in_pos = 0  
+        self.out_pos = 0
+        
+        # Synchronization primitives
+        self.mutex = threading.Lock()
+        self.not_full = threading.Condition(self.mutex)
+        self.not_empty = threading.Condition(self.mutex)
+
+    def add(self, item):
+        with self.not_full:
+            while self.count == self.size:
+                self.not_full.wait()
+
+            self.buffer[self.in_pos] = item
+            self.in_pos = (self.in_pos + 1) % self.size
+            self.count += 1
+
+            self.not_empty.notify()
+
+    def remove(self):
+        with self.not_empty:
+            while self.count == 0:
+                self.not_empty.wait()
+
+            item = self.buffer[self.out_pos]
+            self.out_pos = (self.out_pos + 1) % self.size
+            self.count -= 1
+
+            self.not_full.notify()
+            return item
+
+def worker(context,id):
+    while True:
+        item = context.state['todo_tasks'].remove()
+        if item == context.config['sentinel']:
+            context.state['todo_tasks'].add(item)
+            print(f"Worker {id} received termination signal")
+            break
+        result = item.process_function(item.input_dict)
+        print(f"Worker {id} processing {item.process_function.__name__} request to {result}")
+        context.state['done_tasks'].add(result)
