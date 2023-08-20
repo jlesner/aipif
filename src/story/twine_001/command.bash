875876a44074bfbf654c30bb950adc1c8ccd09f7
jlesner <jlesner@ucsc.edu>
2023-08-19 21:15:32 -0700

    added sugarcube twine template

--- src/story/twine_001/command.bash
+++ src/story/twine_001/command.bash
@@ -1,4 +1,7 @@
-# xsltproc twine_gen.xml story_input.xml > test.twee
+# xsltproc xslt/act_part_remove.xml story_input.xml | xsltproc xslt/chapbook_twine_generate.xml /dev/stdin > test.twee
+# /opt/tweego-2.1.1-linux-x64/tweego -f chapbook-1  -o test.html test.twee
 
-xsltproc xslt/act_part_remove.xml story_input.xml | xsltproc xslt/chapbook_twine_generate.xml /dev/stdin > test.twee
-/opt/tweego-2.1.1-linux-x64/tweego -f chapbook-1  -o test.html test.twee
+# /opt/tweego-2.1.1-linux-x64/tweego -o index.html src
+
+xsltproc xslt/act_part_remove.xml story_input.xml | xsltproc xslt/sugarcube_twine_generate.xml /dev/stdin > test.twee
+/opt/tweego-2.1.1-linux-x64/tweego -f sugarcube-2  -o test.html test.twee
