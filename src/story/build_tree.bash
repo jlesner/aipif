49e4cfc8dfde22c74d09daeb2ed04e74f90a833b
jlesner <jlesner@ucsc.edu>
2023-08-21 15:13:45 -0700

    added sample tree grow flow

--- src/story/build_tree.bash
+++ src/story/build_tree.bash
@@ -1,18 +1,41 @@
 cat structure_0010.xml \
-    | xsltproc xslt_002/p0010_flatten_structure3.xml /dev/stdin \
-    | xsltproc xslt_002/p0020_add_path.xml /dev/stdin \
-    | xsltproc xslt_002/p0025_add_tree.xml /dev/stdin \
-    | xsltproc xslt_002/p0030_build_tree.xml /dev/stdin \
+    | xsltproc xslt_002/p0010_structure_flatten.xml /dev/stdin \
+    \
+    | xsltproc xslt_002/p0020_path_add.xml /dev/stdin \
+    | xsltproc xslt_002/p0025_tree_add.xml /dev/stdin \
+    \
+    | xsltproc xslt_002/p0030_tree_grow.xml /dev/stdin \
+    | xsltproc xslt_002/p0031_response_simulate.xml /dev/stdin \
+    \
+    | xsltproc xslt_002/p0030_tree_grow.xml /dev/stdin \
+    | xsltproc xslt_002/p0032_response_simulate.xml /dev/stdin \
+    \
+    | xsltproc xslt_002/p0030_tree_grow.xml /dev/stdin \
+    | xsltproc xslt_002/p0033_response_simulate.xml /dev/stdin \
+    \
+    | xsltproc xslt_002/p0030_tree_grow.xml /dev/stdin \
+    | xsltproc xslt_002/p0034_response_simulate.xml /dev/stdin \
+    \
+    | xsltproc xslt_002/p0030_tree_grow.xml /dev/stdin \
+    | xsltproc xslt_002/p0035_response_simulate.xml /dev/stdin \
+    \
+    | xsltproc xslt_002/p0030_tree_grow.xml /dev/stdin \
+    | xsltproc xslt_002/p0036_response_simulate.xml /dev/stdin \
+    \
+    | xsltproc xslt_002/p0080_key_add.xml /dev/stdin \
+    | xsltproc xslt_002/p0090_path_drop.xml /dev/stdin \
+    \
     | python3 xml_format.py \
     | grep -v '^<\?xml' \
     | grep -v '^ *$' \
-    | cat \
-    | less -S
+    | cat
 
 exit
-    | xsltproc xslt_002/p0031_simulate_response.xml /dev/stdin \
+    | xsltproc xslt_002/p0090_drop_non_request_detail.xml /dev/stdin \
+    
+    
+    
 
-
-    | xsltproc xslt_002/p0040_build_tree.xml /dev/stdin \
-
-    | perl -pe's/\${emoji_list}/🐧🍕🥄🏝👽/g;' \
+    | grep -v '^[A-Za-z]' \
+    | cut -c 1-`tput cols` \
+    | head -n `tput lines` \
