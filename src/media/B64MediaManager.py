5657a1987583f9d2a52acd0c85a5b3ff0db4fd94
jlesner <jlesner@ucsc.edu>
2023-08-23 05:40:22 -0700

    added S3MediaManager

--- src/media/B64MediaManager.py
+++ src/media/B64MediaManager.py
@@ -1,21 +1,20 @@
 import base64
 
-from ContextAware import ContextAware
+from common.ContextAware import ContextAware
 
 class B64MediaManager(ContextAware):
 
-    def bytes_to_png_url(data:bytes):
+    def bytes_to_png_url(data:bytes) -> str:
         return 'data:image/png;base64,' \
             + base64.b64encode(data).decode('utf-8')
     
-    def bytes_to_jpg_url(data:bytes):
+    def bytes_to_jpg_url(data:bytes) -> str:
         return 'data:image/jpg;base64,' \
             + base64.b64encode(data).decode('utf-8')
-        
-    def binary_file_read(path:str):
-        with open(path, 'rb') as file:
-            return file.read()
-        
-    def binary_file_write(path:str, data:bytes):
-        with open(path, 'wb') as file:
-            file.write(data)
+
+    def url_to_bytes(data_url: str) -> bytes:
+        # if not data_url.startswith('data:image/jpg;base64,'):
+        if not data_url.startswith('data:'):
+            raise ValueError("Invalid data URL format")
+        base64_data = data_url.split(',', 1)[1]
+        return base64.b64decode(base64_data)
--- src/media/FileMediaManager.py
+++ src/media/FileMediaManager.py
@@ -1,6 +1,6 @@
 import base64
 
-from ContextAware import ContextAware
+from common.ContextAware import ContextAware
 
 class FileMediaManager(ContextAware):
         
@@ -11,21 +11,6 @@ class FileMediaManager(ContextAware):
     def binary_file_write(path:str, data:bytes):
         with open(path, 'wb') as file:
             file.write(data)
-
-    def bytes_to_png_url(data:bytes) -> str:
-        return 'data:image/png;base64,' \
-            + base64.b64encode(data).decode('utf-8')
-    
-    def bytes_to_jpg_url(data:bytes) -> str:
-        return 'data:image/jpg;base64,' \
-            + base64.b64encode(data).decode('utf-8')
-
-    def url_to_bytes(data_url: str) -> bytes:
-        # if not data_url.startswith('data:image/jpg;base64,'):
-        if not data_url.startswith('data:'):
-            raise ValueError("Invalid data URL format")
-        base64_data = data_url.split(',', 1)[1]
-        return base64.b64decode(base64_data)
 
 # Testing
 # data = ... # Some bytes of an image
