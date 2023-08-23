d449046016cb2c4f707a2719ad3bec99f752fd4c
jlesner <jlesner@ucsc.edu>
2023-08-23 10:06:12 -0700

    added MediaManager bytes_to_mp4_url()

--- src/media/MediaManager.py
+++ src/media/MediaManager.py
@@ -2,10 +2,15 @@ import base64
 
 class MediaManager:
 
-    def bytes_to_png_url(data:bytes):
+    def bytes_to_png_url(data:bytes) -> str:
         return 'data:image/png;base64,' \
             + base64.b64encode(data).decode('utf-8')
     
-    def bytes_to_jpg_url(data:bytes):
+    def bytes_to_jpg_url(data:bytes) -> str:
         return 'data:image/jpg;base64,' \
             + base64.b64encode(data).decode('utf-8')
+
+    def bytes_to_mp4_url(data:bytes) -> str:
+        return 'data:audio/mp4;base64,' \
+            + base64.b64encode(data).decode('utf-8')
+
