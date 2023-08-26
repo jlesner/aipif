a697b5db516683db5253c94da51e4821c8d713a3
jlesner <jlesner@ucsc.edu>
2023-08-25 18:22:28 -0700

    connecting story_list and story_suggestion, recieving GET requests with flask

--- src/app.py
+++ src/app.py
@@ -2,23 +2,9 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
-from story.seeds.image_seeds import *
-
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-def get_first_emoji_values(emoji_descriptions):
-    first_values = [emoji for emoji, _ in emoji_descriptions]
-    result = " ".join(first_values)
-    return result
-
-@app.route("/emoji", methods=("GET", "POST"))
-def emoji():
-    emoji_list = get_first_emoji_values(emoji_descriptions2)
-
-    return render_template("emoji.html", emoji=str(emoji_list))
-
 
 @app.route("/", methods=("GET", "POST"))
 def index():
@@ -36,24 +22,16 @@ def index():
 
 
 
-@app.route("/story_suggest", methods=("GET", "POST"))
-def index():
-    # if request.method == "POST":
-    #     animal = request.form["animal"]
-    #     response = openai.Completion.create(
-    #         model="text-davinci-003",
-    #         prompt=generate_prompt(animal),
-    #         temperature=0.6,
-    #     )
-    #     # TODO assign story_suggestion from ss URL parameter
+@app.route("/ss", methods=("GET", "POST"))
+def story_suggest():
 
-    story_suggestion = request.args.get("ss")
-                
-    print(f"{story_suggestion} story suggestion" )
+    # TODO assign story_suggestion from ss URL parameter
+    if request.method == "GET":
+        story_suggestion = request.args.get("ss")     
+        print(f"{story_suggestion} story suggestion" )
     # return redirect(url_for("index", result=response.choices[0].text))
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+    return redirect("https://aipif-2023.s3.amazonaws.com/static/story_list.html?m=ty")
 
 
 
