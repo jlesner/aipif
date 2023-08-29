dc74ae04d4fce9fee9b1d45a5f3646100d6ed452
jlesner <jlesner@ucsc.edu>
2023-08-28 23:59:24 -0700

    .

--- ws_launch.bash
+++ ws_launch.bash
@@ -2,4 +2,11 @@ export PYTHONPATH=~/aipif/src
 
 [[ -f .env ]] || echo "make sure you have .env with keys setup ; see .env.example"
 
-flask run
+( 
+    . flask_venv/bin/activate ; 
+    while true ; do 
+        # flask run
+        python3 ~/aipif/src/app.py
+        sleep 10 
+    done
+)
