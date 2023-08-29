f80ccd5e1e60945c145aa7beb45da2e66cb65de8
jlesner <jlesner@ucsc.edu>
2023-08-28 17:44:57 -0700

    updated twine with mute button, story list does all server requests

--- src/twine/twine_gen_cmd.bash
+++ src/twine/twine_gen_cmd.bash
@@ -38,4 +38,6 @@ cat twine/_generated/example_decorated.xml \
 /opt/tweego-2.1.1-linux-x64/tweego -f sugarcube-2 -o /dev/stdout \
     twine/_generated/decorated_tree.twee \
     > twine/_generated/decorated_tree.html
+
+    
     | tee >( aws s3 cp - s3://aipif-2023/sample/twine.html --content-type "text/html" --metadata "Content-Disposition=inline") \
