ebe7e73c26165869f9266da7fcb6ede67b7a16c3
jlesner <jlesner@ucsc.edu>
2024-07-18 13:36:05 -0700

    added new story structures, moved audio buttons, added demo of stable audio open

---
+++ src/sounds/SatDemo.bash
@@ -0,0 +1,5 @@
+time python SatDemo_0020.py birds.wav "The sound of chirping birds filled the air, creating a peaceful and harmonious ambiance."
+time python SatDemo_0020.py chimes.wav  "A gentle tinkling sound filled the air, like the delicate chimes of a wind chime on a breezy day."
+time python SatDemo_0020.py lullaby.wav "A gentle, enchanting melody filled the air, as if the plant itself was singing a sweet lullaby."
+time python SatDemo_0020.py applause.wav "The sound of applause filled the gallery, echoing through the room and enveloping Emily in a wave of appreciation and support."
+time python SatDemo_0020.py leaves.wav "The sound of rustling leaves and the playful chattering of monkeys filled the air, creating a lively and energetic atmosphere."
---
+++ src/sounds/SatDemo_0010.py
@@ -0,0 +1,46 @@
+import torch
+import torchaudio
+from einops import rearrange
+from stable_audio_tools import get_pretrained_model
+from stable_audio_tools.inference.generation import generate_diffusion_cond
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Download model
+model, model_config = get_pretrained_model("stabilityai/stable-audio-open-1.0")
+sample_rate = model_config["sample_rate"]
+sample_size = model_config["sample_size"]
+
+model = model.to(device)
+
+# Set up text and timing conditioning
+conditioning = [{
+    # "prompt": "128 BPM tech house drum loop",
+    "prompt": "The sound of rustling leaves and the playful chattering of monkeys filled the air, creating a lively and energetic atmosphere.",
+    # "prompt": "The sound of chirping birds and rustling leaves filled the air, creating a peaceful and harmonious ambiance.",
+    # "prompt": "A gentle tinkling sound filled the air, like the delicate chimes of a wind chime on a breezy day.",
+    "seconds_start": 0, 
+    "seconds_total": 30
+    # "seconds_total": 10
+}]
+
+# Generate stereo audio
+output = generate_diffusion_cond(
+    model,
+    steps=100,
+    # steps=50,
+    cfg_scale=7,
+    conditioning=conditioning,
+    sample_size=sample_size,
+    sigma_min=0.3,
+    sigma_max=500,
+    sampler_type="dpmpp-3m-sde",
+    device=device
+)
+
+# Rearrange audio batch to a single sequence
+output = rearrange(output, "b d n -> d (b n)")
+
+# Peak normalize, clip, convert to int16, and save to file
+output = output.to(torch.float32).div(torch.max(torch.abs(output))).clamp(-1, 1).mul(32767).to(torch.int16).cpu()
+torchaudio.save("output.wav", output, sample_rate)
